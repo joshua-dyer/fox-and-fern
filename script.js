f9420c537ecbaaefcd26d618e18a85aa55b7f6b6
joshua-dyer <145073496+joshua-dyer@users.noreply.github.com>
2026-02-16 18:37:41 -0600

    Clean up: removing heavy png files for optimized jpg files

--- script.js
+++ script.js
@@ -3,22 +3,31 @@
             const modalImg = document.getElementById("modalImg");
             const modalText = document.getElementById("modalText");
             
-            // Extract the image source and the hidden text from the clicked div
-            const imgSrc = element.getAttribute("data-full");
+            // Get the base name from the data-img-name attribute
+            const imgName = element.getAttribute("data-img-name");
             const titleAndDesc = element.querySelector(".hidden-desc").innerHTML;
             
+            // Define the paths to use.  First the thumb will load.  After the higher-res modal_ images loads, we will swap
+            const thumbPath = `./images/thumbs/${imgName}.jpg`;
+            const fullPath = `./images/gallery/modal_${imgName}.jpg`;
+            
+            // Set up the initial view for the modal
             modalImg.classList.remove('loaded');    // Reset for the next image the modal might use
-            modalImg.onload = function(){
-                modalImg.classList.add('loaded');   // Fade in will begin AFTER image is actually loaded
+            modalText.innerHTML = titleAndDesc;
+            modalImg.src = thumbPath; // Already cached, should load instantly
+            
+            const highRes = new Image();
+            highRes.src = fullPath;
+
+            // after highRes is loaded, swap the thumb for the highRes image
+            highRes.onload = function(){
+                modalImg.src = fullPath;
+                modalImg.classList.add('loaded'); // Triggers CSS fade-in animation
             }
 
-            modalImg.src = imgSrc;
-            modalText.innerHTML = titleAndDesc;
-            
-            // Show the Modal
+            // Show the modal
             modal.style.display = "flex";
-            // Prevent background scrolling while modal is open
-            document.body.style.overflow = "hidden";
+            document.body.style.overflow = "hidden"; // halts scrolling main page while modal is open
 
             // Restart the animation for the content
             const content = modal.querySelector('.modal-content');
